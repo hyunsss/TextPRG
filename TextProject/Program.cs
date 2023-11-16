6dd12a0d9f4a03274151f2352161aade3e4267a6
hyunsss <41880870+hyunsss@users.noreply.github.com>
2023-11-16 12:28:29 +0900

    1

---
+++ TextProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!1");
+        }
+    }
+}
