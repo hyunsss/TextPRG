be074d7c530b38ccbd35f57bea793b0988b60c06
hyunsss <mh81hj1@naver.com>
2023-11-16 12:36:21 +0900

    Update Program.cs
    Text RPG를 만들자!

--- TextProject/Program.cs
+++ TextProject/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace TextProject
+﻿using System;
+
+namespace TextProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("커밋 테스트");
+            Console.WriteLine("TextRPG를 만들자!");
         }
     }
 }
